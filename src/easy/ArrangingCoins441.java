c015982fd55d1a8e7d38d34591a41d2be9dfc3ad
Neka <mdelopez777@gmail.com>
2023-05-21 22:35:58 +0200

    Arranging Coins

---
+++ src/easy/ArrangingCoins441.java
@@ -0,0 +1,11 @@
+package easy;
+
+public class ArrangingCoins441 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int arrangeCoins(int n) {
+        return -1;
+    }
+}

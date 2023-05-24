66d7c572278188bb9c290702ab1a8c377cc5af57
Neka <mdelopez777@gmail.com>
2023-05-24 22:25:33 +0200

    Added Solution to Self Dividing Numbers Added class Move Zeroes

--- src/easy/LC728_SelfDividingNumbers.java
+++ src/easy/LC728_SelfDividingNumbers.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class LC728_SelfDividingNumbers {
     public static void main(String[] args) {
-
+        System.out.println(selfDividingNumbers(10,30));
     }
 
     /**
@@ -42,5 +42,29 @@ public class LC728_SelfDividingNumbers {
     }
     /// </editor-fold>
 
+    /**
+     * Better solution, same approach. <br />
+     * <a href="https://leetcode.com/problems/self-dividing-numbers/solutions/3054038/java-100-faster-explained-properly/">LC Solution</a>
+     */
+    public static List<Integer> selfDividingNumbers2(int left, int right) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            if(checking(i)) list.add(i);
+        }
+        return list;
+    }
+
+    public static boolean checking(int number) {
+        int storingNumber = number;
+        while (number != 0) {
+            int r = number % 10;
+            if (r == 0) return false;
+            if (!(storingNumber % r == 0)) {
+                return false;
+            }
+            number = number / 10;
+        }
+        return true;
+    }
 
 }

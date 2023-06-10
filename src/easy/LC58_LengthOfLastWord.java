d81069fc73cf4ffb329e81c94379b57ed24078c4
Martina <mdelopez777@gmail.com>
2023-06-10 10:13:44 +0200

    Length of last word

---
+++ src/easy/LC58_LengthOfLastWord.java
@@ -0,0 +1,25 @@
+package easy;
+
+public class LC58_LengthOfLastWord {
+
+    /**
+     * <b>1st Try.</b> <br />
+     * String split solution<br />
+     * Solved: 2023-06-10
+     */
+    public int lengthOfLastWord(String s) {
+        String[] words = s.trim().split(" ");
+        return words[words.length -1].length();
+    }
+
+    /**
+     * substring solution<br />
+     * Source: https://leetcode.com/problems/length-of-last-word/solutions/3578178/fastest-shortest-way-to-solve-beats-100/
+     */
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        if(s.indexOf(' ')==-1)
+        return s.length();
+        return s.substring(s.lastIndexOf(' ')).length()-1;
+    }
+}

230b6210d070b1db04101930552a7ad0e2bf19b4
Neka <mdelopez777@gmail.com>
2023-07-07 23:49:34 +0200

    Minimize Confusion of Exam

---
+++ src/medium/LC2024_MinimizeConfusionOfExam.java
@@ -0,0 +1,25 @@
+package medium;
+
+public class LC2024_MinimizeConfusionOfExam {
+    /**
+     * lee215's solution <br />
+     * I used a bit of help because it is very late<br />
+     * Homework: Understand what this does
+     */
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        int result = 0;
+        int maxFreq = 0;    // Frequency of the character that occurs more times
+        int[] count = new int[128];
+
+
+        for (int i = 0; i < answerKey.length(); i++) {
+            maxFreq = Math.max(maxFreq, ++count[answerKey.charAt(i)]);
+            if (result - maxFreq < k) {
+                result++;
+            } else {
+                count[answerKey.charAt(i - result)]--;
+            }
+        }
+        return result;
+    }
+}

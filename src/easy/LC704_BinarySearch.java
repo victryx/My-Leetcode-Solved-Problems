c6d3cab69d811ddb1149e02fd678910c30eeaa1f
Neka <mdelopez777@gmail.com>
2023-07-11 20:20:22 +0200

    Binary Search

---
+++ src/easy/LC704_BinarySearch.java
@@ -0,0 +1,54 @@
+package easy;
+
+public class LC704_BinarySearch {
+
+    public static void main(String[] args) {
+        int n = new LC704_BinarySearch().search(new int[] {-1,0,3,5,9,12}, 32);
+        System.out.println(n);
+    }
+
+    /**
+     * <b>1st Try.</b> <br />
+     * Iterative Binary Search<br />
+     * Solved: 2023-07-11
+     */
+    public int search(int[] nums, int target){
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * <b>1st Try.</b> <br />
+     * Recursive Binary Search<br />
+     * Solved: 2023-07-11
+     */
+    public int searchRecursive(int[] nums, int target) {
+        return search2(nums, target, nums.length - 1, 0);
+    }
+    public int search2(int[] nums, int target, int hi, int lo) {
+        if (lo >= hi) {
+            return -1;
+        }
+        int mid = (hi + lo) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        }
+        if (nums[mid] < target) {
+            return search2(nums, target, hi, mid + 1);
+        }
+        
+        return search2(nums, target, mid - 1, lo);
+    }
+}
--- src/medium/LC853_CarFleet.java
+++ src/medium/LC853_CarFleet.java
@@ -4,7 +4,6 @@ import java.util.Stack;
 
 public class LC853_CarFleet {
     public int carFleet(int target, int[] position, int[] speed) {
-        Stack<Integer[]>
 
         return 1;
     }
